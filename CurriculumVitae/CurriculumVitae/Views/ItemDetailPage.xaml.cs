423ea0bad54d8ed1b1f86b13f58c2df780e11564
JoseLuisColcha <jose.colcha@epn.edu.ec>
2022-07-24 16:46:40 -0500

    Agregar archivos de proyecto.

---
+++ CurriculumVitae/CurriculumVitae/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CurriculumVitae.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CurriculumVitae.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ CurriculumVitae/CurriculumVitae/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CurriculumVitae.Models;
+using CurriculumVitae.ViewModels;
+using CurriculumVitae.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CurriculumVitae.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
